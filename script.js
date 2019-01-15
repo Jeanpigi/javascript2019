0e0972d75cefa70caa9e8a788b087ec9e8a71745
Jean Pierre <stick-master666@hotmail.com>
2019-01-15 17:47:13 -0500

    aprendiendo a utilizar las variables y los bucles

---
+++ script.js
@@ -0,0 +1,11 @@
+//entrada al cine
+var edad = 20, mayoriaEdad, dias, seguir = true;
+// uso operador condicional
+mayoriaEdad = (edad >= 18) ?  "Si! es mayor de Edad" : mayoriaEdad = "No! es mayor de Edad";
+// mostramos el no para la entrada
+console.log(mayoriaEdad);
+// Sentencia For
+
+for (var dias = 1; dias <= 7; dias++) {
+  console.log(dias);
+}
